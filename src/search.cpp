50d4b2f2adb6cf16aec28374fc4364629637cabe
M.Z <zerbinati.marco@gmail.com>
2019-11-16 21:22:30 +0100

    Prune before extension
    Switch execution order in search: do move pruning before extension
    detection.
    
    STC:
    LLR: 2.96 (-2.94,2.94) [-1.50,4.50]
    Total: 5762 W: 1307 L: 1181 D: 3274
    http://tests.stockfishchess.org/tests/view/5dcc56e90ebc59025bcbb833
    
    LTC:
    LLR: 2.96 (-2.94,2.94) [0.00,3.50]
    Total: 72956 W: 11959 L: 11585 D: 49412
    http://tests.stockfishchess.org/tests/view/5dcc62840ebc59025bcbb96f
    
    Closes #2413
    
    Bench: 4532366
    master (#1139)
    Stefan Geschwentner
    @snicolet
    Stefan Geschwentner

--- src/search.cpp
+++ src/search.cpp
@@ -1122,12 +1122,59 @@ moves_loop: // When in check, search starts from here
       captureOrPromotion = pos.capture_or_promotion(move);
       movedPiece = pos.moved_piece(move);
       givesCheck = pos.gives_check(move);
+      //full threads patch begin
       if(thisThread->fullSearch)
       {
-	  newDepth = depth - 1 + extension;
+		  newDepth = depth - 1;
           goto skipExtensionAndPruning;
       }
-      // Step 13. Extensions (~70 Elo)
+      //full threads patch end
+
+      // Calculate new depth for this move
+      newDepth = depth - 1;
+
+      // Step 13. Pruning at shallow depth (~170 Elo)
+      if (  !rootNode
+          && pos.non_pawn_material(us)
+          && bestValue > VALUE_MATED_IN_MAX_PLY)
+      {
+          //from Kelly begin
+          if (isSingularExtension && moveCount > 1)
+          {
+          	continue;
+		  }
+		  //from Kelly end
+          // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
+          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+
+          if (   !captureOrPromotion
+              && !givesCheck
+              && (!PvNode || !pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
+          {
+              // Reduced depth of the next LMR search
+              int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
+
+              // Countermoves based pruning (~20 Elo)
+              if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
+                  && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
+                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
+                  continue;
+
+              // Futility pruning: parent node (~2 Elo)
+              if (   lmrDepth < 6
+                  && !inCheck
+                  && ss->staticEval + 250 + 211 * lmrDepth <= alpha)
+                  continue;
+
+              // Prune moves with negative SEE (~10 Elo)
+              if (!pos.see_ge(move, Value(-(31 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
+                  continue;
+          }
+          else if (!pos.see_ge(move, Value(-199) * depth)) // (~20 Elo)
+                  continue;
+      }
+
+      // Step 14. Extensions (~70 Elo)
 
       // Singular extension search (~60 Elo). If all moves but one fail low on a
       // search of (alpha-s, beta-s), and just one fails high on (alpha, beta),
@@ -1154,17 +1201,12 @@ moves_loop: // When in check, search starts from here
           {
               extension = 1;
               singularLMR = true;
-
-              if (value < singularBeta - std::min(4 * depth, 36))
-				{
-                  singularLMR = true;
-				}	
-				  //from Kelly begin
-				  if(persistedSelfLearning && expTTHit && move == expTTMove)
-				  	{
-				      isSingularExtension = true;
-				    }
-				  //from Kelly end
+			  //from Kelly begin
+			  if(persistedSelfLearning && expTTHit && move == expTTMove)
+			  	{
+			      isSingularExtension = true;
+			    }
+			  //from Kelly end
           }
 
           // Multi-cut pruning
@@ -1192,51 +1234,9 @@ moves_loop: // When in check, search starts from here
       if (type_of(move) == CASTLING)
           extension = 1;
 
-      // Calculate new depth for this move
-      newDepth = depth - 1 + extension;
+      // Add extension to new depth
+      newDepth += extension;
 
-      // Step 14. Pruning at shallow depth (~170 Elo)
-      if (  !rootNode
-          && pos.non_pawn_material(us)
-          && bestValue > VALUE_MATED_IN_MAX_PLY)
-      {
-          // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
-
-          if (   !captureOrPromotion
-              && !givesCheck
-              && (!PvNode || !pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
-          {
-              //from Kelly begin
-              if (isSingularExtension && moveCount > 1)
-	      	  {
-            	  continue;
-	      	  }
-              //from Kelly end
-			  
-              // Reduced depth of the next LMR search
-              int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
-
-              // Countermoves based pruning (~20 Elo)
-              if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
-                  && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
-                  continue;
-
-              // Futility pruning: parent node (~2 Elo)
-              if (   lmrDepth < 6
-                  && !inCheck
-                  && ss->staticEval + 250 + 211 * lmrDepth <= alpha)
-                  continue;
-
-              // Prune moves with negative SEE (~10 Elo)
-              if (!pos.see_ge(move, Value(-(31 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
-                  continue;
-          }
-          else if (  !(givesCheck && extension)
-                   && !pos.see_ge(move, Value(-199) * depth)) // (~20 Elo)
-                  continue;
-      }
 
       skipExtensionAndPruning: //full threads search patch
 
