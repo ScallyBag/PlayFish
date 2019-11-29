54b7fb67e4c8b85da74bc84c258efb9bbdc64d23
M.Z <zerbinati.marco@gmail.com>
2019-11-29 10:25:05 +0100

    Do lmr for more captures
    Based on machinery introduced by vondele. Logic behind patch if
    relatively simple -
    if we reduce less with high hit rate of transposition table somewhat
    logical is to
    reduce more with low hit rate. For example enable all captures for LMR.
    
    Threshold 0.375 is arbitrary and can be tweaked :)
    
    STC
    http://tests.stockfishchess.org/tests/view/5dd4d51df531e81cf278eaac
    LLR: 2.97 (-2.94,2.94) [-1.50,4.50]
    Total: 16495 W: 3591 L: 3434 D: 9470
    
    LTC
    http://tests.stockfishchess.org/tests/view/5dd52265f531e81cf278eace
    LLR: 2.96 (-2.94,2.94) [0.00,3.50]
    Total: 23598 W: 3956 L: 3716 D: 15926
    
    Closes #2420
    
    Bench: 5067870
    master (#1148)
    @Vizvezdenec
    @snicolet
    Vizvezdenec authored

--- src/search.cpp
+++ src/search.cpp
@@ -1275,7 +1275,8 @@ moves_loop: // When in check, search starts from here
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || cutNode))
+              || cutNode
+              || thisThread->ttHitAverage < 384 * ttHitAverageResolution * ttHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
 
